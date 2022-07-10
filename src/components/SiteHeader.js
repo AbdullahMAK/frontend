c80d928977462bfd3a6eb66a59ad8de9effdfcb5
AbdullahMAK <47482822+AbdullahMAK@users.noreply.github.com>
2022-07-10 06:57:28 +0300

    two Folders

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function SiteHeader() {
     return (
         <div className="site-header">
-            <Link to="/"><h1> Abdllah's Reviews</h1></Link>
+            <Link to="/"><h1> Abdllah's Reviews WebSite:</h1></Link>
         </div>
     )
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import useFatch from '../Hooks/useFetch'
+import { Link } from 'react-router-dom'
 
 export default function Homepage() {
+  const{ loading, error, data } = useFatch('http://http://localhost:1337/reviews')
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error...</p>
   return (
   <div>
-    <h1> Hello </h1>
+    {data.data.map((review) => (
+      <div key={review.attributes.id} className="review-card" >
+      <div className="rating">{review.rating}</div>
+      <h2>{review.title}</h2>
+
+      <small>Console list</small>
+
+      <p>{review.body}</p>
+      <Link to={`/details/${review.id}`}>Read more...</Link>
+      </div>
+    ))}
   </div>
   )
 }
